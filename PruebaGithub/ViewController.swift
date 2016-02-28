9376ea923f040f48902d7035648463b6cfa8452c
Jason Solano <jason.s@hotmail.es>
2016-02-27 23:05:14 -0600

    Initial Commit

---
+++ PruebaGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGithub
+//
+//  Created by Jason Solano on 27/2/16.
+//  Copyright © 2016 Jason Solano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
